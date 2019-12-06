d87f6cd76f8141113d53e64369edd1569f859eaf
dave <spamduck321@gmail.com>
2019-12-06 16:07:52 +0000

    Fade in-out

---
+++ Assets/Scripts/fadeFromBlack.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class fadeFromBlack : MonoBehaviour
+{
+    public float FadeSpeed = 2f;
+
+    Image img;
+    // Start is called before the first frame update
+    void Start()
+    {
+        img = this.GetComponent<Image>();
+        this.GetComponent<RectTransform>().localScale = new Vector2(Screen.width, Screen.height);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (img.color.a >= 0.05f)
+        {
+            img.color = Color.Lerp(img.color, Color.clear, FadeSpeed * Time.deltaTime);
+        }
+        else
+        {
+            img.color = Color.clear;
+        }
+    }
+}
