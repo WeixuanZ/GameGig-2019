f602eea924d8b70af4035314ee16da681ede0eac
AntrosEco <antroseconomides@hotmail.co.uk>
2019-12-06 11:03:57 +0000

    Player: initial commit

---
+++ Assets/Player/keyboard_input.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class keyboard_input : MonoBehaviour
+{
+    public float Speed = 10;
+    public Rigidbody2D Body;
+
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 dr = new Vector3(h, v, 0);
+        dr = dr.normalized * Speed * Time.deltaTime;
+
+        Body.MovePosition(Body.transform.position + dr);
+    }
+}
