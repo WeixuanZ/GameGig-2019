4e949f3657c9258c298059d1906010424770a291
Felix Chippendale <felix.chippendale@gmail.com>
2019-12-06 14:31:25 +0000

    Added sound effects to menu

--- Assets/Scripts/MouseOverButton.cs
+++ Assets/Scripts/MouseOverButton.cs
@@ -3,19 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using UnityEngine.Audio;
 
 public class MouseOverButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     public ParticleSystem system;
+    public AudioClip menuNoise;
+    public AudioSource musicSource;
 
     void Start()
     {
         system.Stop();
+        musicSource.clip = menuNoise;
     }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
         system.Play();
+        musicSource.Play();
     }
 
     public void OnPointerExit(PointerEventData eventData)
