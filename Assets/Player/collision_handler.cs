45ee6b2965e4bacde5a83e55c6eb0090badb0f3e
AntrosEco <antroseconomides@hotmail.co.uk>
2019-12-06 14:01:49 +0000

    Player: Freeze on collision

--- Assets/Player/collision_handler.cs
+++ Assets/Player/collision_handler.cs
@@ -3,9 +3,13 @@
 public class collision_handler : MonoBehaviour
 {
     public ParticleSystem ExplosionHandle;
+    public Rigidbody2D BodyHandle;
 
     void OnCollisionEnter2D(Collision2D collision)
     {
+        // Freeze the ship
+        BodyHandle.constraints = RigidbodyConstraints2D.FreezeAll;
+        // Explode
         ExplosionHandle.Play();
         // Destroy the Box Collider 2D object so that no more collisions are registered
         Destroy(gameObject);
