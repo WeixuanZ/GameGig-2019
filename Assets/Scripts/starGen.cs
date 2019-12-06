0c811e206923e3acf2235533eb89920575102c80
dave <spamduck321@gmail.com>
2019-12-06 17:40:37 +0000

    Star gen improvement

--- Assets/Scripts/starGen.cs
+++ Assets/Scripts/starGen.cs
@@ -6,6 +6,9 @@ public class starGen : MonoBehaviour
 {
     public GameObject starPre;
     public Camera cam;
+
+    public float scrollSpeed;
+
     private List<GameObject> stars;
     private List<float> starDist;
     // Start is called before the first frame update
@@ -13,6 +16,14 @@ public class starGen : MonoBehaviour
     {
         stars = new List<GameObject>();
         starDist = new List<float>();
+
+        Vector3 top = cam.ViewportToWorldPoint(new Vector3(1, 1, 10));
+        Vector3 bottom = cam.ViewportToWorldPoint(new Vector3(0, 0, 10));
+        while (stars.Count < 1000)
+        {
+            float x = bottom.x + Random.value * (top.x - bottom.x);
+            GenAtX(x, bottom.y, top.y);
+        }
     }
 
     // Update is called once per frame
@@ -23,13 +34,7 @@ public class starGen : MonoBehaviour
 
         if(stars.Count < 1000)
         {
-            float yRange = top.y - bottom.y;
-
-            GameObject star = Instantiate(starPre, new Vector3(top.x, bottom.y + yRange * Random.value, 10), Quaternion.identity);
-            float starDistF = Random.value;
-            star.transform.localScale *= starDistF;
-            stars.Add(star);
-            starDist.Add(starDistF);
+            GenAtX(top.x, bottom.y, top.y);
         }
         for(int i= stars.Count-1; i>=0; i--)
         {
@@ -39,7 +44,17 @@ public class starGen : MonoBehaviour
                 stars.RemoveAt(i);
                 starDist.RemoveAt(i);
             }
-            stars[i].transform.position -= new Vector3(starDist[i] * starDist[i], 0, 0)*Time.deltaTime*30;
+            stars[i].transform.position -= new Vector3(starDist[i] * starDist[i], 0, 0)*Time.deltaTime* scrollSpeed;
         }
+    }
+
+    void GenAtX(float x, float yMin, float yMax)
+    {
+        float yRange = yMax - yMin;
+        GameObject star = Instantiate(starPre, new Vector3(x, yMin + yRange * Random.value, 10), Quaternion.identity);
+        float starDistF = Random.value;
+        star.transform.localScale *= starDistF;
+        stars.Add(star);
+        starDist.Add(starDistF);
     }
 }
