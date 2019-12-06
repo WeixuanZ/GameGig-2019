29afdc41ea1877fcd03ba32a4f0d3afc04f4e991
AntrosEco <antroseconomides@hotmail.co.uk>
2019-12-06 14:39:58 +0000

    Player: Flash on death

--- Assets/Player/collision_handler.cs
+++ Assets/Player/collision_handler.cs
@@ -1,17 +1,34 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class collision_handler : MonoBehaviour
 {
     public ParticleSystem ExplosionHandle;
     public Rigidbody2D BodyHandle;
+    public SpriteRenderer SpriteHandle;
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        // Freeze the ship
+        /*
+        * Disable the Box Collider 2D component so that no more collisions are registered
+        * If the object was destroyed instead, then the coroutine below wouldn't run
+        */
+        GetComponent<BoxCollider2D>().enabled = false;
+
         BodyHandle.constraints = RigidbodyConstraints2D.FreezeAll;
-        // Explode
         ExplosionHandle.Play();
-        // Destroy the Box Collider 2D object so that no more collisions are registered
-        Destroy(gameObject);
+
+        StartCoroutine(FlashShip());
+    }
+
+    IEnumerator FlashShip()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            yield return new WaitForSeconds(0.2f);
+            SpriteHandle.enabled = !SpriteHandle.enabled;
+        }
+
+        SpriteHandle.enabled = false;
     }
 }
