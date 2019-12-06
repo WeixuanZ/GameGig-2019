ae23338fb99292b1acfa02eced7f70e08e151a69
dave <spamduck321@gmail.com>
2019-12-06 20:04:55 +0000

    highscores fix

---
+++ Assets/mainMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject.FindGameObjectWithTag("persistent").GetComponent<SceneData>().fromGame = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
