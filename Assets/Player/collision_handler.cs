74ef53b615f05e6a063d9a15cacb33de40398a8e
AntrosEco <antroseconomides@hotmail.co.uk>
2019-12-06 13:57:07 +0000

    Player: Explode on contact

--- Assets/Player/collision_handler.cs
+++ Assets/Player/collision_handler.cs
@@ -2,8 +2,12 @@
 
 public class collision_handler : MonoBehaviour
 {
+    public ParticleSystem ExplosionHandle;
+
     void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log("You died");
+        ExplosionHandle.Play();
+        // Destroy the Box Collider 2D object so that no more collisions are registered
+        Destroy(gameObject);
     }
 }
