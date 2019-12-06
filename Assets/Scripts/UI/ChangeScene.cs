be297272996911703af600963712f866b78716d3
AntrosEco <antroseconomides@hotmail.co.uk>
2019-12-06 16:47:50 +0000

    Menu changes

---
+++ Assets/Scripts/UI/ChangeScene.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    public string ChangeTo;
+
+    void Start()
+    {
+        this.GetComponent<Button>().onClick.AddListener(Transition);
+    }
+
+    void Transition()
+    {
+        SceneManager.LoadScene(this.ChangeTo, LoadSceneMode.Single);
+    }
+}
