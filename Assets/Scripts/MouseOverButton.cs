efab7e5562e496163419e69493e06825340041fc
Felix Chippendale <felix.chippendale@gmail.com>
2019-12-06 13:42:05 +0000

    No Comments

---
+++ Assets/Scripts/MouseOverButton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class MouseOverButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public ParticleSystem system;
+
+    void Start()
+    {
+        system.Stop();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        system.Play();
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        system.Stop();
+    }
+}
