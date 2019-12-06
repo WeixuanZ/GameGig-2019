191c86f8e70b06f960a99317c78c0f672767c749
dave <spamduck321@gmail.com>
2019-12-06 13:01:26 +0000

    No Comments

---
+++ Assets/Scripts/CountDown.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class CountDown : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void ShowNumber(int number, float speed)
+    {
+        this.GetComponent<Text>().text = number.ToString();
+    }
+}
