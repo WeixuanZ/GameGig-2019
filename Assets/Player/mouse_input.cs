ce5eb0837e9434d1ea6150b031b19ee7b1c24abf
AntrosEco <antroseconomides@hotmail.co.uk>
2019-12-06 14:19:05 +0000

    Player: Add 10% margin on each edge

--- Assets/Player/mouse_input.cs
+++ Assets/Player/mouse_input.cs
@@ -4,7 +4,7 @@ public class mouse_input : MonoBehaviour
 {
     public Rigidbody2D Body;
     public BoxCollider2D Collider;
-    public float Margin = 0;
+    public float Margin = 0.10f; // Percentage, for each edge of the screen
 
     private float Xmin;
     private float Xmax;
@@ -14,13 +14,14 @@ public class mouse_input : MonoBehaviour
     void Start()
     {
         Vector2 Character = Collider.size;
-        Vector3 World = Camera.main.ScreenToWorldPoint(
-            new Vector3(Screen.width, Screen.height, 0));
+        Vector3 World = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
+        float MarginX = World.x * Margin;
+        float MarginY = World.y * Margin;
 
         // Assumes the Camera is centered at the Origin
-        Xmin = -World.x + Margin + Character.x / 2;
+        Xmin = -World.x + MarginX + Character.x / 2;
         Xmax = -Xmin;
-        Ymin = -World.y + Margin + Character.y / 2;
+        Ymin = -World.y + MarginY + Character.y / 2;
         Ymax = -Ymin;
     }
 
