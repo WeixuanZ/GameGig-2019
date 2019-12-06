8fb8abc87deeca84c051815bd00560e14d772601
AntrosEco <antroseconomides@hotmail.co.uk>
2019-12-06 13:05:53 +0000

    Player: Convert to prefab and add to MainGame

---
+++ Assets/Player/collision_handler.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class collision_handler : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log(collision);
+        /*if (collision.gameObject.tag == "Enemy")
+        {
+            collision.gameObject.SendMessage("ApplyDamage", 10);
+        }*/
+    }
+}
