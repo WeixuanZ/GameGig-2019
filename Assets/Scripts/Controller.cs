cb04db545831ad1968050f0f3a82a2e955b6e6e3
dave <spamduck321@gmail.com>
2019-12-06 11:02:40 +0000

    No Comments

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class Controller : MonoBehaviour
 {
+    public Camera camera;
+    public GameObject bullet;
+
+    public float innerRect;
     // Start is called before the first frame update
     void Start()
     {
-        
+        SpawnBullets();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+    }
+
+    void SpawnBullets()
+    {
+        Vector3 top = camera.ViewportToWorldPoint(new Vector3(0, 0, 0));
+        Vector3 bottom = camera.ViewportToWorldPoint(new Vector3(1, 1, 0));
+
+        Instantiate(bullet, top, Quaternion.identity);
+        Instantiate(bullet, bottom, Quaternion.identity);
+    }
+
+    void GeneratePath(Vector3 top, Vector3 bottom)
+    {
+
     }
 }
